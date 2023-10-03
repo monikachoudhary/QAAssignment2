86b6def7adfd31d057f202f03192a0ab09691d73
Monika <mchoudhary0198@conestogac.on.ca>
2023-10-03 18:04:31 -0400

    Second Commit

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -22,4 +22,51 @@ public class Tests
         //Assert
         Assert.AreEqual("An equilateral triangle is formed", result);
     }
+
+
+    [Test]
+    public void AnalyzeTriangleS1_IsoscelesSides_ReturnsIsosceles()
+    {
+        //Arrange
+        int side1 = 4;
+        int side2 = 5;
+        int side3 = 5;
+
+        //Act
+        var result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+        //Assert
+        Assert.AreEqual("An isosceles triangle is formed", result);
+    }
+
+    [Test]
+    public void AnalyzeTriangleSide2_IsoscelesSides_ReturnsIsosceles()
+    {
+        //Arrange
+        int side1 = 5;
+        int side2 = 4;
+        int side3 = 5;
+
+        //Act
+        var result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+        //Assert
+        Assert.AreEqual("An isosceles triangle is formed", result);
+    }
+
+    [Test]
+    public void AnalyzeTriangleSide3_IsoscelesSides_ReturnsIsosceles()
+    {
+        //Arrange
+        int side1 = 5;
+        int side2 = 5;
+        int side3 = 4;
+
+        //Act
+        var result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+        //Assert
+        Assert.AreEqual("An isosceles triangle is formed", result);
+    }
+
 }
